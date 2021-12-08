1940061386a21d654ff4eb5877410b0b793a14bb
wenrui <17502227423@163.com>
2021-12-08 14:35:36 +0800

    2nd

--- src/main/java/HolleWorld.java
+++ src/main/java/HolleWorld.java
@@ -1,5 +1,11 @@
 public class HolleWorld {
     public static void main(String[] args) {
         System.out.println("H");
+        System.out.println("12344");
+
+        run();
+    }
+    public static void run(){
+        System.out.println("run");
     }
 }
